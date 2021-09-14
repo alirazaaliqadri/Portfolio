58b75b9fa54535ae87f9a809893e31f25085a6dc
Ali Raza Ali Qadri <ali_333347@yahoo.com>
2021-09-14 12:02:16 +0500

    UI fixes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,6 @@
-import { Grid, Paper, TextField, Typography } from '@material-ui/core'
-import { MailOutlineRounded } from '@material-ui/icons';
+import { Button, Grid, Paper, TextField, Typography } from '@material-ui/core'
+import MailOutlineTwoTone from '@material-ui/icons/MailOutlineTwoTone';
+
 import React from 'react'
 import "./Components.css";
 export default function Contact() {
@@ -9,70 +10,66 @@ export default function Contact() {
         setValue(event.target.value);
     };
     return (
-        <div className="container-fluid contactContainer" style={{ marginTop: "25vh", marginLeft: "auto", marginRight: "auto", display: "block",  }} >
+        <div className="contactMainDiv">
+            <br />
+            <br />
+            <br />
+            <Typography variant="h4" component="h4">Contact Me</Typography>
 
-            <Grid
-                container spacing={1}
-                direction="row"
-                justifyContent="flex-start"
-                alignItems="center">
-                <Grid item xs={3}>
-                    <Paper elevation={3} className="container-fluid contactDiv1">
-                        <Grid direction="column"
-                            justifyContent="left"
-                            alignItems="flex-start">
-                            <Grid item xs={12}>
-                                <Typography variant="h4" component="h4">Contact Information</Typography>
+            <div className="container-fluid contactContainer" >
+                <Grid
+                    container spacing={3}>
+                    <Grid item xs={12} sm={3}>
+                        <Paper elevation={3} className="container contactDiv1">
+                            <Grid container spacing={3}>
+                                <Grid item item xs={12} sm={12}>
+                                    <Typography variant="h4" component="h4">Contact Information</Typography>
+                                </Grid>
+                                <Grid item item xs={12} sm={12}>
+                                    <Typography variant="subtitle2">
+                                        Fill the form to contact me
+                                    </Typography>
+                                </Grid>
+                                <Typography variant="body2" component="h4" variant="h2" style={{ fontSize: 'small', fontWeight: "bold" }}>
+                                    <MailOutlineTwoTone style={{ fontSize: 'small', marginRight: '2px' }} />
+                                    {'alirazaaliqadri@gmail.com'}</Typography>
                             </Grid>
-                            <Grid item xs={12}>
-                                <Typography variant="subtitle2">
-                                    Fill the form to contact me
-                                </Typography>
+                        </Paper>
+                    </Grid>
+
+                    <Grid item xs={12} sm={8} >
+                        <form name="contact" method="post" netlify-data="true" autoComplete="off">
+                            <Grid container spacing={3}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField label="First Name" type="name" color="primary" id="filled-required" />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField label="Last Name" color="primary" type="name" id="filled-required" />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField type="email" label="Email" id="filled-required" />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField label="Phone" type="tel" id="filled-required" />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField id="outlined-multiline-flexible"
+                                        className="messageField"
+                                        label="Multiline"
+                                        multiline
+                                        maxRows={4}
+                                        onChange={handleChange}
+                                        variant="outlined" type="text" label="Write your message" />
+                                </Grid>
                             </Grid>
                             <br />
                             <br />
-                            <MailOutlineRounded size="sm" />alirazaaliqadri@gmail.com
-                        </Grid>
-                    </Paper>
-                </Grid>
-                <Grid item xs={7}>
+                            <Button onClick="submit" color="default" variant="contained" name="submit" className="submitBtn" fontSize="small">Submit</Button>
+                        </form>
 
-                    <form name="contact" method="post" netlify-data="true" autoComplete="off">
-                        <Grid
-                            direction="row"
-                            container spacing={1}>
-                            <Grid item xs>
-                                <TextField label="First Name" type="name" color="primary" id="filled-required" />
-                            </Grid>
-                            <Grid item xs>
-                                <TextField label="Last Name" color="primary" type="name" id="filled-required" />
-                            </Grid>
-                        </Grid>
-                        <Grid
-                            direction="row"
-                            container spacing={1}>
-                            <Grid item xs>
-                                <TextField type="email" label="Email" id="filled-required" />
-                            </Grid>
-                            <Grid item xs>
-                                <TextField label="Phone" type="tel" id="filled-required" />
-                            </Grid>
-                            <Grid item xs={12} float="left">
-                                <br />
-                                <TextField id="outlined-multiline-flexible"
-                                    label="Multiline"
-                                    multiline
-                                    maxRows={4}
-                                    style={{ marginLeft: "30px", width: "60vh" }}
-                                    value={value}
-                                    onChange={handleChange}
-                                    variant="outlined" type="text" label="Write your message" />
-                            </Grid>
-                        </Grid>
-                    </form>
-
+                    </Grid>
                 </Grid>
-            </Grid>
+            </div >
         </div>
     )
 }
